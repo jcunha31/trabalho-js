3963e309594b552523d820df1d241d68a7e79a5b
joao cunha <a22201470@mso365.ulp.pt>
2022-12-11 23:11:07 +0000

    variáveis exercicio1

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 console.log("olá")
+
+console.log(7 * age)
+
+let friends = ["João", "André", "Débora"];
+let numbers = [1, 2, 3];
+console.log(friends[1]);
+
+let person = {
+    name: "João",
+    age: 19,
+    isStudent: true
+  };
+  console.log(person.name);
+
+let students = [
+    {
+      name: "João",
+      age: 18,
+      isStudent: true
+    },
+    {
+      name: "André",
+      age: 18,
+      isStudent: true
+    },
+    {
+      name: "Débora",
+      age: 18,
+      isStudent: true
+    }
+  ];
+  console.log(students[0].name);
